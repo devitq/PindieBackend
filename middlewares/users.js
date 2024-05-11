f6f9ba3f0c50a7c4cf3a8ec71655e74f440a0fa7
ITQ <itq.dev@ya.ru>
2024-05-11 11:47:19 +0300

    Hidden passwords from endpoints

--- middlewares/users.js
+++ middlewares/users.js
@@ -20,7 +20,7 @@ const createUser = async (req, res, next) => {
 const findAllUsers = async (req, res, next) => {
   console.log("GET /users");
 
-  req.usersArray = await users.find({});
+  req.usersArray = await users.find({}, { password: 0 });
 
   next();
 };
@@ -29,7 +29,7 @@ const findUserById = async (req, res, next) => {
   console.log("GET /users/:id");
 
   try {
-    req.user = await users.findById(req.params.id);
+    req.user = await users.findById(req.params.id, { password: 0 });
     next();
   } catch (error) {
     res.setHeader("Content-Type", "application/json");
