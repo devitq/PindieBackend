b1644b59c5f9361b53d5288740bf59cb36061fbd
ITQ <itq.dev@ya.ru>
2024-05-11 11:18:56 +0300

    Added start and config files

---
+++ config.js
@@ -0,0 +1,10 @@
+// config.js
+
+const PORT = 3000;
+const DB_URL = "mongodb+srv://pindie:nqmQuBaiP2CSwU7G@cluster0.gqq4i1y.mongodb.net/pindie";
+const CORS = [
+  "https://practicum.yandex.ru",
+  "https://students-projects.ru",
+];
+
+module.exports = { PORT, DB_URL, CORS };
---
+++ index.js
@@ -0,0 +1,29 @@
+// index.js
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const { DB_URL, PORT } = require("./config");
+
+const cors = require("./middlewares/cors");
+const { connectToDatabase } = require("./database/connect");
+
+const { categoriesRouter, gamesRouter, usersRouter } = require("./routes");
+
+const app = express();
+
+connectToDatabase(DB_URL);
+
+app.use(
+  cors,
+  bodyParser.json(),
+  express.static(path.join(__dirname, "public")),
+  categoriesRouter,
+  gamesRouter,
+  usersRouter
+);
+
+app.listen(PORT, () => {
+  console.log(`Приложение запущено на: http://localhost:${PORT}`);
+});
