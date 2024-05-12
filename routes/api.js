3da981e820b26abd6225472f832fb7857ea6b840
ITQ <itq.dev@ya.ru>
2024-05-12 13:52:28 +0300

    Added ping route

--- routes/api.js
+++ routes/api.js
@@ -5,11 +5,13 @@ const apiRouter = require("express").Router();
 const authRouter = require("./auth");
 const categoriesRouter = require("./categories");
 const gamesRouter = require("./games");
+const pingRouter = require("./ping");
 const usersRouter = require("./users");
 
 apiRouter.use("/api", authRouter);
 apiRouter.use("/api", gamesRouter);
 apiRouter.use("/api", usersRouter);
+apiRouter.use("/api", pingRouter);
 apiRouter.use("/api", categoriesRouter);
 
 module.exports = apiRouter;
---
+++ routes/ping.js
@@ -0,0 +1,9 @@
+// routes/ping.js
+
+const pingRouter = require("express").Router();
+
+pingRouter.get("/ping", (req, res) => {
+    res.send("pong");
+})
+
+module.exports = pingRouter;
