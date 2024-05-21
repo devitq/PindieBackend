40c0d6f4250d3ef60c0766001dfa86e83f070b2f
ITQ <itq.dev@ya.ru>
2024-05-21 14:15:10 +0300

    Fixed cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -7,6 +7,14 @@ function cors(req, res, next) {
 
   if (CORS.includes(origin)) {
     res.header("Access-Control-Allow-Origin", origin);
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET,HEAD,PUT,PATCH,POST,DELETE"
+    );
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
+    );
   }
 
   next();
